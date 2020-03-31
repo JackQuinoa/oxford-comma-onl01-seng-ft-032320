59ba31187c589e304c45e85ab151b3f69b61fd59
Jack Quinoa <jquinoa208@gmail.com>
2020-03-31 21:15:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require "pry"
 def oxford_comma(array)
-
+  if array.count==1
+    return array[0]
+  elsif array.count==2
+    return array.join(" and ")
+  else 
+    array[-1].insert(0, "and ")
+    return array.join(", ")
+  end
 end
