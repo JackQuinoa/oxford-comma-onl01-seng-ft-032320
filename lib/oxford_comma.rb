18479de8fdffc6bc2aa7de99bc64223c7d4a9ac2
Jack Quinoa <jquinoa208@gmail.com>
2020-03-31 21:05:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
+require "pry"
 def oxford_comma(array)
   if array.count==1
     return array[0]
   elsif array.count==2
     return array.join(" and ")
   else 
-    return array.insert(-2, " and ").join(",")
+    binding.pry
+    return array[-1].insert(-2, " and ").join(",")
   end
 end
